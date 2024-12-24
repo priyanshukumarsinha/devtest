4e8f500372208aaeb7a538d6a39b3b44f8931f91
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-12-24 09:46:31 +0000

    app done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+});
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
+});
